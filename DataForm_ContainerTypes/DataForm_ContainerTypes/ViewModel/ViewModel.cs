3d242978d259465284603bda249b756f0862fe7a
AjitK <ajithkumar.senthilkumar@syncfusion.com>
2019-06-27 12:56:10 +0530

    Added DataForm_ContainerTypes sample.

---
+++ DataForm_ContainerTypes/DataForm_ContainerTypes/ViewModel/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataForm_ContainerTypes
+{
+    /// <summary>
+    /// Represents the employee form information of the data form in this sample.
+    /// </summary>
+    public class ViewModel
+    {
+        private EmployeeForm employeeForm;
+        public EmployeeForm EmployeeForm
+        {
+            get { return this.employeeForm; }
+            set { this.employeeForm = value; }
+        }
+        public ViewModel()
+        {
+            this.employeeForm = new EmployeeForm();
+        }
+    }
+}
